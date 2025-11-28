4acdb1335744f3d605e3377936a6d74348abedc7
obsqure223 <giuffridagiuseppeemanuele@gmail.com>
2025-11-29 00:25:50 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-from interface import TrisFletUI
-
-def main(page: ft.Page):
-    # Inizializza l'interfaccia utente
-    TrisFletUI(page)
-
-if __name__ == "__main__":
-    ft.app(target=main, assets_dir="assets")
